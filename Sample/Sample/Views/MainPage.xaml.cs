1157ebfd53364b08d4abbdadec5177bf160b1808
Juliano Custodio <juliano.custodio@hotmail.com.br>
2019-07-27 14:55:25 -0300

    Create project

---
+++ Sample/Sample/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Sample.ViewModels;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private MainViewModel ViewModel => BindingContext as MainViewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
